5ade04635db5c7f4e395ccd9914ed4cd02d69c83
kor <kor@dwarshuis.com>
2024-06-26 06:05:04 +0200

    Create a timestamped backup of xrefs info

---
+++ output/xrefs-history/xrefs-data-1719311361381.js
@@ -0,0 +1,28 @@
+const allXrefs = {
+  "xrefs": [
+    {
+      "externalSpec": "test-1",
+      "term": "Aal",
+      "repoUrl": "https://github.com/kordwarshuis/spec-up-xref-test-1",
+      "terms_dir": "spec/term-definitions",
+      "owner": "kordwarshuis",
+      "repo": "spec-up-xref-test-1",
+      "site": "https://kordwarshuis.github.io/spec-up-xref-test-1/",
+      "commitHash": [
+        "58f0665866cdc8d5232362c3c64eb541721e06f9"
+      ]
+    },
+    {
+      "externalSpec": "test-2",
+      "term": "Abac",
+      "repoUrl": "https://github.com/kordwarshuis/spec-up-xref-test-2",
+      "terms_dir": "spec/term-definitions",
+      "owner": "kordwarshuis",
+      "repo": "spec-up-xref-test-2",
+      "site": "https://kordwarshuis.github.io/spec-up-xref-test-2/",
+      "commitHash": [
+        "117b86fa676c0bd3030c16e8bbb87ebaa61af576"
+      ]
+    }
+  ]
+};
